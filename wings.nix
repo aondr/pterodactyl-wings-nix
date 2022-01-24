3355425a8d49ac4eb0ca50b97e7c0de332d8ee57
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2022-01-24 11:59:36 +0200

    pterodactyl-wings: 1.5.3 -> 1.5.6

--- wings.nix
+++ wings.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "pterodactyl-wings";
-  version = "1.5.3";
+  version = "1.5.6";
 
   src = fetchFromGitHub {
     owner = "pterodactyl";
     repo = "wings";
     rev = "v${version}";
-    sha256 = "sha256-2Tdx37/rojpj2d9Pm7KV6MFNveYmEqbP94HaJuwT4O4=";
+    sha256 = "sha256-SzN31Cl63f4iduGJMJ4/H3jggdME7UX1fTriwqEbsxM=";
   };
 
-  vendorSha256 = "sha256-QE5nBhWRMJRyYbipviDjVaZmHorVQrDhNHahGKSvfa0=";
+  vendorSha256 = "sha256-nPRBN+wmj9dszYMAGkMsk/sENK7xWae306BOQbDaa2Q=";
   subPackages = [ "." ];
 
   ldflags = [
