7071e2d460ffa572c767bf83e76bf59bb49becdc
realweezerfan93 <22036195+rwf93@users.noreply.github.com>
2022-11-20 06:58:38 -0800

    pterodactyl-wings: 1.6.1 -> 1.7.2

--- wings.nix
+++ wings.nix
@@ -2,16 +2,16 @@
 
 buildGo118Module rec {
   pname = "pterodactyl-wings";
-  version = "1.6.1";
+  version = "1.7.2";
 
   src = fetchFromGitHub {
     owner = "pterodactyl";
     repo = "wings";
     rev = "v${version}";
-    sha256 = "sha256-erqt+/2RTQOOwIyg+YzFNLYTVn8OHQ9tZ7tN4rwDLHs=";
+    sha256 = "sha256-TZVALOXOFZg9Dau76sXAl1+xucM9HHomL6OnZ1fBXZQ=";
   };
 
-  vendorSha256 = "sha256-JqIeBGTtgpyUrKySEADt4oy5FgVzheBOIgpVQVxnzsE=";
+  vendorSha256 = "sha256-3/gCc7akZ+rDugqf2BhwSrPbRnCej9k0WWTc5/NLDmY=";
   subPackages = [ "." ];
 
   ldflags = [
