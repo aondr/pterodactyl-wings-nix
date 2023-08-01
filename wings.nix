7c38a9e7d152aed561e97aa71c4643019616cf46
rwf93 <aidendscheller@gmail.com>
2023-08-01 14:03:04 -0700

    update to v1.11.7

--- wings.nix
+++ wings.nix
@@ -5,16 +5,16 @@
 }:
 buildGo120Module rec {
   pname = "pterodactyl-wings";
-  version = "v1.11.6";
+  version = "v1.11.7";
 
   src = fetchFromGitHub {
     owner = "pterodactyl";
     repo = "wings";
     rev = "${version}";
-    sha256 = "sha256-lDJ0/yxmS7+9TVp3YpEdQQb12R4i2GyQ0w6RXoC5NHs=";
+    sha256 = "sha256-MWeUSlJsKNnV/oZotIne3KN3j+fjKNfPJQb+DbhNYLk=";
   };
 
-  vendorSha256 = "sha256-VApv+VSot/GmOyU3pBlOvHYG0oE3fCtTxN5F3PsYYf0=";
+  vendorSha256 = "sha256-3jJQQEb2N28dhXJg5knzZgfnwXUOqnouRvedpTY23jw=";
   subPackages = ["."];
 
   ldflags = [
