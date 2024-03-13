f9b6340034c0a2d5d986e0f640dffb3b02be148b
rwf93 <22036195+rwf93@users.noreply.github.com>
2024-03-13 16:13:37 -0700

    updated to latest

--- wings.nix
+++ wings.nix
@@ -5,13 +5,13 @@
 }:
 buildGo121Module rec {
   pname = "pterodactyl-wings";
-  version = "v1.11.9";
+  version = "v1.11.10";
 
   src = fetchFromGitHub {
     owner = "pterodactyl";
     repo = "wings";
     rev = "${version}";
-    sha256 = "sha256-GyaGwC7X/qpyeCoE6k2Ljo1PeaGVWYWEC5idNBJBVik=";
+    sha256 = "sha256-EOkHi+x4ciODyHPPx7766IEh8XGLHY4Ng1N/iq2mgJI=";
   };
 
   vendorHash = "sha256-LR4JuV73BWhXI97HGNH93Hd5NMU9PD84Rqfz4GOJzUs=";
