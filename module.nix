851d4eeb5d076cb4bfe563b5c4374ebd7dab1fe1
Saturn <90934664+Saturn745@users.noreply.github.com>
2024-03-23 23:23:19 +0000

    Small module fixes

--- module.nix
+++ module.nix
@@ -19,12 +19,12 @@ in {
     user = lib.mkOption {
       type = lib.types.str;
       description = lib.mdDoc "The user to run the Pterodactyl Wings daemon as";
-      default = "wings";
+      default = "pterodactyl";
     };
     group = lib.mkOption {
       type = lib.types.str;
       description = lib.mdDoc "The group to run the Pterodactyl Wings daemon as";
-      default = "wings";
+      default = "pterodactyl";
     };
     tokenFile = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
@@ -63,7 +63,7 @@ in {
       }
     ];
 
-    users.users = lib.optionalAttrs (cfg.user == "wings") {
+    users.users = lib.optionalAttrs (cfg.user == "pterodactyl") {
       wings = {
         name = "wings";
         group = cfg.group;
@@ -71,7 +71,7 @@ in {
       };
     };
 
-    users.groups = lib.optionalAttrs (cfg.group == "wings") {
+    users.groups = lib.optionalAttrs (cfg.group == "pterodactyl") {
       wings = {
         name = "wings";
       };
@@ -81,6 +81,7 @@ in {
     systemd.tmpfiles.rules = [
       "d /var/log/pterodactyl 0700 ${cfg.user} ${cfg.group}"
       "d /var/lib/pterodactyl 0700 ${cfg.user} ${cfg.group}"
+      "d /etc/pterodactyl 0700 ${cfg.user} ${cfg.group}"
     ];
 
     systemd.services.wings = {
