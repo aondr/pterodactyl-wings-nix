cdc743bcb41faf414a9198ef3d50ed6a2c680255
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2024-04-05 16:46:46 -0700

    pterodactyl-wings: 1.11.10 -> 1.11.11
    Diff: https://github.com/pterodactyl/wings/compare/v1.11.10...v1.11.11
    
    Changelog: https://github.com/pterodactyl/wings/releases/tag/v1.11.11

--- wings.nix
+++ wings.nix
@@ -5,13 +5,13 @@
 }:
 buildGo121Module rec {
   pname = "pterodactyl-wings";
-  version = "1.11.10";
+  version = "1.11.11";
 
   src = fetchFromGitHub {
     owner = "pterodactyl";
     repo = "wings";
     rev = "v${version}";
-    sha256 = "sha256-EOkHi+x4ciODyHPPx7766IEh8XGLHY4Ng1N/iq2mgJI=";
+    sha256 = "sha256-7rpnPKeFZM6QZfiI1Vp7ibCdBNTOo7stf8lNZlN4I4M=";
   };
 
   vendorHash = "sha256-LR4JuV73BWhXI97HGNH93Hd5NMU9PD84Rqfz4GOJzUs=";
