d6e661a9cc83cabf9039959e1615d720eea9c47b
Adam <adam@ondrejcak.sk>
2025-07-08 11:37:02 +0200

    remove pterodactyl wings (deprecated), bump up go module build version

--- flake.nix
+++ flake.nix
@@ -15,17 +15,12 @@
     ];
     forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
   in {
-    nixosModules = {
-      pterodactyl-wings = import ./module.nix self;
-      default = self.nixosModules.pterodactyl-wings;
-    };
     packages = forAllSystems (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        pterodactyl-wings = pkgs.callPackage ./pterodactyl-wings.nix {};
         pelican-wings = pkgs.callPackage ./pelican-wings.nix {};
-        default = self.packages.${system}.pterodactyl-wings;
+        default = self.packages.${system}.pelican-wings;
       }
     );
   };
--- pelican-wings.nix
+++ pelican-wings.nix
@@ -1,20 +1,20 @@
 {
   lib,
-  buildGo121Module,
+  buildGo123Module,
   fetchFromGitHub,
 }:
-buildGo121Module rec {
+buildGo123Module rec {
   pname = "pelican-wings";
-  version = "1.0.0-beta3";
+  version = "1.0.0-beta14";
 
   src = fetchFromGitHub {
     owner = "pelican-dev";
     repo = "wings";
     rev = "v${version}";
-    sha256 = "sha256-sNmiOpgwG4Qx6vVFdj2zr/4LFrLCNpyVFq83gXQoBOM=";
+    sha256 = "sha256-SDPHngoVp1k5vAzyMdCpeTtbkthtLxwazZPUD5Y2+KI=";
   };
 
-  vendorHash = "sha256-M7ymgLdYbQ4ejmO4xPBKtXHVhv8dJvE/I+exedCyyL8=";
+  vendorHash = "sha256-a4FmIFd9CcvfXn8+qHAvROxmQCsOGjm/5a+tm4SSmzw=";
   subPackages = ["."];
 
   ldflags = [
